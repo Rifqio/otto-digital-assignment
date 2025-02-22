6c26d6d67ba0b23eeeac5afc528379189339e553
Rifqi Oktario <oktariorifqi@gmail.com>
2025-02-22 17:20:05 +0700

    fix(validation): change max character

--- frontend/src/schema/greeting-form.schema.tsx
+++ frontend/src/schema/greeting-form.schema.tsx
@@ -4,12 +4,12 @@ export const greetingFormSchema = z.object({
     dear: z
         .string()
         .nonempty({ message: "Recipient must be filled" })
-        .max(50, { message: "Recipient must be less than 50 characters" }),
+        .max(20, { message: "Recipient must be less than 50 characters" }),
     message: z
         .string()
         .nonempty({ message: "Message must be filled" })
         .max(50, { message: "Message must be less than 200 characters" }),
-    from: z.string().nonempty({ message: "From must be filled" }).max(50, { message: "From must be less than 50 characters" }),
+    from: z.string().nonempty({ message: "From must be filled" }).max(20, { message: "From must be less than 50 characters" }),
     card: z.instanceof(FileList, { message: "Card must be uploaded" }),
 });
 
